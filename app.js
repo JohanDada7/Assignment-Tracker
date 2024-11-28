34d3da46dd98001be1e64c95d41f344a5646f8d1
JohanDada7 <Johan.dada@ontariotechu.net>
2024-11-28 17:28:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Flash Messages
 app.use(flash());
 
-// Global Variables for Flash Messages
-app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success');
-  res.locals.error_msg = req.flash('error');
-  next();
-});
+
 
 // Routes
 const assignmentRoutes = require('./routes/assignmentRoutes');
